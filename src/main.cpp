2c161173a4120a644fc0b0c627e599ea9fe7301a
Luke McCulloch <tlukemcculloch@gmail.com>
2019-03-28 23:06:45 -0500

    some basic tests of expression template elementwise add

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "input.hpp"
 #include "tests_array.hpp"
 
+#include "tests_etarray.hpp"
+
 using namespace std;
 
 int main (int argc, char **argv) {
@@ -15,7 +17,21 @@ int main (int argc, char **argv) {
     SolverManager SM = SolverManager();
     SM.driver(argc, argv);
 
-    //arraybasic();
+
+    char type;
+    do
+    {
+        cout << "Run some (quick!) tests? (y/n)" << endl;
+        cin >> type;
+    }
+    while( !cin.fail() && type!='y' && type!='n' );
+
+    if ( type =='y' ) {
+        //array basic tests
+        arraybasic();
+        //expression tempalte (et) array basic tests
+        etarraybasic();
+    }
 
     return 0;
 }
---
+++ src/tests_etarray.hpp
@@ -0,0 +1,37 @@
+//=================================
+// include guard
+#ifndef __TESTS_ETARRAY_INCLUDED__
+#define __TESTS_ETARRAY_INCLUDED__
+
+//=================================
+// forward declared dependencies
+
+//=================================
+// included dependencies
+
+//=================================
+// the actual function
+void etarraybasic();
+
+// class TestArray:
+//     public BaseTest01<float, float, 
+//                         float, bool>{
+//     public:
+//         TestArray(); //default constructor
+//         void
+//         apply(const float &l1,
+//             const float &l2,
+//             const float &l3, bool &result)
+//         {
+//             Triangle triangle;
+//             result =
+//             triangle.setLengths(l1, l2, l3);
+//         }
+//         const char *getName() const
+//         {
+//             return "Test setLengths";
+//         }
+// };
+
+#endif //__TESTS_ETARRAY_INCLUDED__
+
