799cb8204b5eb2f3ba8ea7765e449b6ba8be062b
Luke McCulloch <Luke.McCulloch@bentley.com>
2019-04-01 18:07:19 -0500

    considering adding more expression template operation classes...

--- src/etops1.hpp
+++ src/etops1.hpp
@@ -183,3 +183,80 @@ class MatMult {
           return op1.getncols()!=0 ? op1.getncols() : op2.getncols();
       }
 };
+
+
+
+// class for objects that represent the matrix multiplication of two operands
+template <typename T, typename OP1, typename OP2>
+class MatDotVec {
+  private:
+    typename A_Traits<OP1>::ExprRef op1;    // first operand
+    typename A_Traits<OP2>::ExprRef op2;    // second operand
+
+    //template <typename T, Rep = Array2D<T> >
+  public:
+
+    //ExprRef cache_result;
+    //implement_matmul;
+    // constructor initializes references to operands
+    MatDotVec (OP1 const& a, OP2 const& b)
+    : op1(a), op2(b) {
+      //implement_matmul = matmul;
+       //cache_result = matmul(op1,op2);
+    }
+
+
+    // auto operator=(){
+    //   auto c(i,k) += op1.array(i,j) * op2.array(j,k);
+    //   return c
+    // }
+
+    // compute product when value requested
+    // auto operator=() const {
+    //     auto result = matmul(op1 , op2) ;
+    //     return result(i,j);
+    // }
+
+    // compute product when value requested
+    // OP1 matmul () {
+    //     return matmul(op1,op2) ;
+    // }
+    
+
+    // compute product when value requested
+    T operator() (size_t i) const {
+      T temp = 0.;
+      for (size_t k = 0; k < op1.getncols(); k++) {
+        temp += op1(i, k) * op2(k);
+      }
+      return temp;
+    }
+
+    // template <typename C>
+    // void assign_to(C&& c) const {
+
+    //     auto& op1 = this->op1();
+    //     auto& op2 = this->op2();
+
+    // }
+
+    // size is maximum size
+    size_t size() const {
+        assert (op1.size()==0 || op2.size()==0
+                || op1.size()==op2.size());
+        return op1.size()!=0 ? op1.size() : op2.size();
+    }
+
+    
+      size_t getnrows() const {
+          assert (op1.getnrows()==0 || op2.getnrows()==0
+                || op1.getnrows()==op2.getnrows());
+          return op1.getnrows()!=0 ? op1.getnrows() : op2.getnrows();
+      }
+
+      size_t getncols() const {
+          assert (op1.getncols()==0 || op2.getncols()==0
+                || op1.getncols()==op2.getncols());
+          return op1.getncols()!=0 ? op1.getncols() : op2.getncols();
+      }
+};
