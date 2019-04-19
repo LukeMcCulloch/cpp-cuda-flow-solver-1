510ccba2eada1cb2f4ad0db16cdb31f4c5223bc7
Luke McCulloch <tlukemcculloch@gmail.com>
2019-04-19 05:48:16 -0500

    machine stats up front

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,77 @@
 
 using namespace std;
 
+
+
+
+void devprops(cudaDeviceProp deviceProp, int dev){
+
+    int driverVersion = 0, runtimeVersion = 0;
+    cudaDriverGetVersion(&driverVersion);
+    cudaRuntimeGetVersion(&runtimeVersion);
+    printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
+           driverVersion / 1000, (driverVersion % 100) / 10,
+           runtimeVersion / 1000, (runtimeVersion % 100) / 10);
+    printf("  CUDA Capability Major/Minor version number:    %d.%d\n",
+           deviceProp.major, deviceProp.minor);
+    printf("  Total amount of global memory:                 %.2f GBytes (%llu "
+           "bytes)\n", (float)deviceProp.totalGlobalMem / pow(1024.0, 3),
+           (unsigned long long)deviceProp.totalGlobalMem);
+    printf("  GPU Clock rate:                                %.0f MHz (%0.2f "
+           "GHz)\n", deviceProp.clockRate * 1e-3f,
+           deviceProp.clockRate * 1e-6f);
+    printf("  Memory Clock rate:                             %.0f Mhz\n",
+           deviceProp.memoryClockRate * 1e-3f);
+    printf("  Memory Bus Width:                              %d-bit\n",
+           deviceProp.memoryBusWidth);
+
+    if (deviceProp.l2CacheSize)
+    {
+        printf("  L2 Cache Size:                                 %d bytes\n",
+               deviceProp.l2CacheSize);
+    }
+
+    printf("  Max Texture Dimension Size (x,y,z)             1D=(%d), "
+           "2D=(%d,%d), 3D=(%d,%d,%d)\n", deviceProp.maxTexture1D,
+           deviceProp.maxTexture2D[0], deviceProp.maxTexture2D[1],
+           deviceProp.maxTexture3D[0], deviceProp.maxTexture3D[1],
+           deviceProp.maxTexture3D[2]);
+    printf("  Max Layered Texture Size (dim) x layers        1D=(%d) x %d, "
+           "2D=(%d,%d) x %d\n", deviceProp.maxTexture1DLayered[0],
+           deviceProp.maxTexture1DLayered[1], deviceProp.maxTexture2DLayered[0],
+           deviceProp.maxTexture2DLayered[1],
+           deviceProp.maxTexture2DLayered[2]);
+    printf("  Total amount of constant memory:               %lu bytes\n",
+           deviceProp.totalConstMem);
+    printf("  Total amount of shared memory per block:       %lu bytes\n",
+           deviceProp.sharedMemPerBlock);
+    printf("  Total number of registers available per block: %d\n",
+           deviceProp.regsPerBlock);
+    printf("  Warp size:                                     %d\n",
+           deviceProp.warpSize);
+    printf("  Maximum number of threads per multiprocessor:  %d\n",
+           deviceProp.maxThreadsPerMultiProcessor);
+    printf("  Maximum number of threads per block:           %d\n",
+           deviceProp.maxThreadsPerBlock);
+    printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
+           deviceProp.maxThreadsDim[0],
+           deviceProp.maxThreadsDim[1],
+           deviceProp.maxThreadsDim[2]);
+    printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
+           deviceProp.maxGridSize[0],
+           deviceProp.maxGridSize[1],
+           deviceProp.maxGridSize[2]);
+    printf("  Maximum memory pitch:                          %lu bytes\n",
+           deviceProp.memPitch);
+    //exit(EXIT_SUCCESS);
+}
+
+
+
+
+
+
+
 int main (int argc, char **argv) {
 
 
@@ -27,6 +98,9 @@ int main (int argc, char **argv) {
     CHECK(cudaGetDeviceProperties(&deviceProp, dev));
     printf("using Device %d: %s\n", dev, deviceProp.name);
     CHECK(cudaSetDevice(dev));
+
+
+    devprops(deviceProp, dev);
 
 
     SolverManager SM = SolverManager();
--- tests/tests_etarray.cu
+++ tests/tests_etarray.cu
@@ -125,89 +125,18 @@ void helloFromCpu(){
 
 
 
-void devprops(int dev, 
-                int driverVersion, 
-                int runtimeVersion,
-                cudaDeviceProp deviceProp){
-//     cudaDriverGetVersion(&driverVersion);
-//     cudaRuntimeGetVersion(&runtimeVersion);
-//     printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
-//            driverVersion / 1000, (driverVersion % 100) / 10,
-//            runtimeVersion / 1000, (runtimeVersion % 100) / 10);
-//     printf("  CUDA Capability Major/Minor version number:    %d.%d\n",
-//            deviceProp.major, deviceProp.minor);
-//     printf("  Total amount of global memory:                 %.2f GBytes (%llu "
-//            "bytes)\n", (float)deviceProp.totalGlobalMem / pow(1024.0, 3),
-//            (unsigned long long)deviceProp.totalGlobalMem);
-//     printf("  GPU Clock rate:                                %.0f MHz (%0.2f "
-//            "GHz)\n", deviceProp.clockRate * 1e-3f,
-//            deviceProp.clockRate * 1e-6f);
-//     printf("  Memory Clock rate:                             %.0f Mhz\n",
-//            deviceProp.memoryClockRate * 1e-3f);
-//     printf("  Memory Bus Width:                              %d-bit\n",
-//            deviceProp.memoryBusWidth);
-
-//     if (deviceProp.l2CacheSize)
-//     {
-//         printf("  L2 Cache Size:                                 %d bytes\n",
-//                deviceProp.l2CacheSize);
-//     }
-
-//     printf("  Max Texture Dimension Size (x,y,z)             1D=(%d), "
-//            "2D=(%d,%d), 3D=(%d,%d,%d)\n", deviceProp.maxTexture1D,
-//            deviceProp.maxTexture2D[0], deviceProp.maxTexture2D[1],
-//            deviceProp.maxTexture3D[0], deviceProp.maxTexture3D[1],
-//            deviceProp.maxTexture3D[2]);
-//     printf("  Max Layered Texture Size (dim) x layers        1D=(%d) x %d, "
-//            "2D=(%d,%d) x %d\n", deviceProp.maxTexture1DLayered[0],
-//            deviceProp.maxTexture1DLayered[1], deviceProp.maxTexture2DLayered[0],
-//            deviceProp.maxTexture2DLayered[1],
-//            deviceProp.maxTexture2DLayered[2]);
-//     printf("  Total amount of constant memory:               %lu bytes\n",
-//            deviceProp.totalConstMem);
-//     printf("  Total amount of shared memory per block:       %lu bytes\n",
-//            deviceProp.sharedMemPerBlock);
-//     printf("  Total number of registers available per block: %d\n",
-//            deviceProp.regsPerBlock);
-//     printf("  Warp size:                                     %d\n",
-//            deviceProp.warpSize);
-//     printf("  Maximum number of threads per multiprocessor:  %d\n",
-//            deviceProp.maxThreadsPerMultiProcessor);
-//     printf("  Maximum number of threads per block:           %d\n",
-//            deviceProp.maxThreadsPerBlock);
-//     printf("  Maximum sizes of each dimension of a block:    %d x %d x %d\n",
-//            deviceProp.maxThreadsDim[0],
-//            deviceProp.maxThreadsDim[1],
-//            deviceProp.maxThreadsDim[2]);
-//     printf("  Maximum sizes of each dimension of a grid:     %d x %d x %d\n",
-//            deviceProp.maxGridSize[0],
-//            deviceProp.maxGridSize[1],
-//            deviceProp.maxGridSize[2]);
-//     printf("  Maximum memory pitch:                          %lu bytes\n",
-//            deviceProp.memPitch);
-//     exit(EXIT_SUCCESS);
-}
-
-
 
 int etarraybasic (){
 
 
-    /* set up device: 
-       nvcc required! */
-//     int dev = 0, driverVersion = 0, runtimeVersion = 0;
-//     cudaDeviceProp deviceProp;
-//     CHECK(cudaGetDeviceProperties(&deviceProp, dev));
-//     printf("Using Device %d: %s\n", dev, deviceProp.name);
-//     CHECK(cudaSetDevice(dev));
-
-
 
     int np = 10;
-    //int nrows = np;
-    //int ncols = np;
+
+    //expression template matrices:
     Array<float> a(np,np), b(np,np), c(np,np), d(np,np);
 
+
+    //compare with traditional overloaded matrices:
     Array2D<float> x(np,np), y(np,np), z(np,np), w(np,np);
 
     // initialize arrays with some values
@@ -374,7 +303,7 @@ int etarraybasic (){
     compare(b,y,"b,y");
 
     //c = matmul(d,c);
-    c = matmul((c+.5f*d),d); //still no support for combined & cached terms
+    c = matmul((c+.5f*d),d); //combined & cached terms are supported
     std::cout << "after c=matmul((1.+b),c) ";
     print(c);
 
@@ -385,7 +314,7 @@ int etarraybasic (){
     compare(d,w,"d,w");
 
     //z = matmul(w,z);
-    z = matmul((z+.5f*w),w); //still no support for zomyinew & zxzhew terms
+    z = matmul((z+.5f*w),w); //compare with traditional overload
     std::cout << "xfter z=matmul((1.+y),z) ";
     print(z);
 
@@ -436,8 +365,10 @@ int etarraybasic (){
 
     helloFromCpu();
     helloFromGPUthread<<<1, 10>>>();
+
     CHECK(cudaDeviceReset());
 
+    //exit(EXIT_SUCCESS);
     return 0;
 
 }
